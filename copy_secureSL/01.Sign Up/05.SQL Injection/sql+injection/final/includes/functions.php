f0c99df853e70ea629814d4ea0bbdc9f5143056e
b7short <mblogwebsite@gmail.com>
2020-09-21 12:19:45 -0400

    Deleted

--- copy_secureSL/01.Sign Up/05.SQL Injection/sql+injection/final/includes/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    function escape($string) {
-        global $connection;
-        return mysqli_real_escape_string($connection, $string);
-    }
--- copy_secureSL/01.Sign Up/14.Insert User Data Into Database/inserting+data+into+database/starter/password_hashing.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $pass = "secret";
-    $hash = password_hash($pass, PASSWORD_BCRYPT, ['cost'=>10]);
-
-    if(password_verify($pass, $hash)) {
-        echo "Password matched";
-    } else {
-        echo "Sorry wrong password";
-    }
